6dc894e1252f3a487163bc40a12a625a8dbaee24
Dmitry <drnecromant@gmail.com>
2016-12-12 13:43:03 +0300

    Add class DBUserStats as a model
    That class provide user stats such as
    min, max, avg, sum, count
    
    Essensial for main algorythm

--- models.py
+++ models.py
@@ -13,3 +13,70 @@ class DBUser(db.Model):
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
+
+class DBUserStats(object):
+	"""
+	Get stats from DBUser such as:
+	min, max, average, total with where clause
+	"""
+
+	def __init__(self, offsetX = None, offsetY = None, \
+				limitX = None, limitY = None):
+		query = db.session.query(
+			db.func.min(DBUser.x).label("minX"), \
+			db.func.min(DBUser.y).label("minY"), \
+			db.func.max(DBUser.x).label("maxX"), \
+			db.func.max(DBUser.y).label("maxY"), \
+			db.func.avg(DBUser.x).label("avgX"), \
+			db.func.avg(DBUser.y).label("avgY"), \
+			db.func.sum(DBUser.x).label("sumX"), \
+			db.func.sum(DBUser.y).label("sumY"), \
+			db.func.count(DBUser.id).label("count"), \
+		)
+
+		if offsetX:
+			query.filter_by(x >= offsetX)
+		if offsetY:
+			query.filter_by(y >= offsetY)
+		if limitX:
+			query.filter_by(x <= limitX)
+		if limitY:
+			query.filter_by(y <= limitY)
+
+		self.result = query.one()
+
+	@property
+	def minX(self):
+		return self.result.minX
+
+	@property
+	def minY(self):
+		return self.result.minY
+
+	@property
+	def maxX(self):
+		return self.result.maxX
+
+	@property
+	def maxY(self):
+		return self.result.maxY
+
+	@property
+	def avgX(self):
+		return int(self.result.avgX)
+
+	@property
+	def avgY(self):
+		return int(self.result.avgY)
+
+	@property
+	def sumX(self):
+		return self.result.sumX
+
+	@property
+	def sumY(self):
+		return self.result.sumY
+
+	@property
+	def count(self):
+		return self.result.count
